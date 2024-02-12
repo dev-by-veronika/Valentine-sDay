eb7453d501f5d0929e642cc09ef244880f50603e
Veronika Duma <dumaveronika2002@gmail.com>
2024-02-12 23:51:09 +0100

    first commit

--- script.js
+++ script.js
@@ -32,3 +32,4 @@ noButton.addEventListener('click', function() {
   }
 });
 
+
---
+++ script2.js
@@ -0,0 +1,3 @@
+document.querySelector('.container-2').addEventListener('click', function() {
+  this.classList.toggle('.card');
+});
